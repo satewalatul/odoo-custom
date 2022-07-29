03846422872da38c65bbb5a12528446fdc2fd62e
Ajay Mahato <ajjayymahato@gmail.com>
2022-07-29 12:17:38 +0530

    LQ / Branch Added

--- youngman/models/opportunity_inherit.py
+++ youngman/models/opportunity_inherit.py
@@ -11,3 +11,11 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     job_order = fields.Char(string="Job Order")
+
+    lead_qual = fields.Many2one(
+        'res.users', string='Lead Qualifier', default=lambda self: self.env.user,
+        domain="['&', ('share', '=', False), ('company_ids', 'in', user_company_ids)]",
+        check_company=True, index=True, tracking=True)
+
+
+    branch = fields.Many2one('jobsite.godown', string="Branch")
