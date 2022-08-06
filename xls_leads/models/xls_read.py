9fd4587a656f4cfd01fdd207935df0a00d04fe8b
Ajay Mahato <ajjayymahato@gmail.com>
2022-08-06 15:58:47 +0530

    xls updated

--- xls_leads/models/xls_read.py
+++ xls_leads/models/xls_read.py
@@ -1,8 +1,5 @@
 from __future__ import print_function
-import json
 import traceback
-from google.oauth2 import service_account
-from google.auth.transport.requests import AuthorizedSession
 from odoo import fields, models, api
 import pygsheets
 
@@ -20,29 +17,10 @@ class ReadXls(models.TransientModel):
         print("checking")
         print("Executing")
 
-        # secret_dict = {
-        #         "type": self.env['ir.config_parameter'].sudo().get_param('xls_leads.type'),
-        #         "project_id": self.env['ir.config_parameter'].sudo().get_param('xls_leads.project_id'),
-        #         "private_key_id": self.env['ir.config_parameter'].sudo().get_param('xls_leads.private_key_id'),
-        #         "client_email": self.env['ir.config_parameter'].sudo().get_param('xls_leads.client_email'),
-        #         "client_id": self.env['ir.config_parameter'].sudo().get_param('xls_leads.client_id'),
-        #         "auth_uri": self.env['ir.config_parameter'].sudo().get_param('xls_leads.auth_uri'),
-        #         "token_uri": self.env['ir.config_parameter'].sudo().get_param('xls_leads.token_uri'),
-        #         "auth_provider_x509_cert_url": self.env['ir.config_parameter'].sudo().get_param('xls_leads.auth_provider_x509_cert_url'),
-        #         "client_x509_cert_url": self.env['ir.config_parameter'].sudo().get_param('xls_leads.client_x509_cert_url')
-        #     }
-        # SCOPES = ('https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive')
-        # s1 = json.dumps(secret_dict)
-        # service_account_info = json.loads(s1)
-        # my_credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
-        # client = pygsheets.authorize(custom_credentials=my_credentials)
-
         client = pygsheets.authorize(service_account_file='odoo-custom/xls_leads/models/keys.json')
         sheet1 = client.open_by_url('https://docs.google.com/spreadsheets/d/1joEMBnP87NFMrB0N11C0SzqvKYmn0CxKYv4hvQh2yUA')
         worksheet = sheet1.sheet1
         cells = worksheet.get_all_records(empty_value='', head=1, majdim='ROWS')
-
-
 
         end_row = len(cells)
         leads = [{
@@ -53,7 +31,6 @@ class ReadXls(models.TransientModel):
             'lead_qual': lead['your_name'],
             'lead_qual_num': lead['your_number']
         } for lead in cells]
-        print(leads)
 
         for lead in leads:
             try:
