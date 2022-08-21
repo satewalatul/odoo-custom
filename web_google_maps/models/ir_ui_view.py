d9f789ace53bb9a29fa3a44578661ee55f928dd2
Ajay Mahato <ajjayymahato@gmail.com>
2022-08-21 21:35:55 +0530

    deleted web_google_maps

--- web_google_maps/models/ir_ui_view.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3
-from odoo import fields, models
-
-
-class IrUiView(models.Model):
-    _inherit = 'ir.ui.view'
-
-    type = fields.Selection(selection_add=[('google_map', 'Google Maps')])
